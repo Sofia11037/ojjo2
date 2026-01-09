b693c29bdba9ca0112fe7f07ecf1e45440937728
Sofia11037 <113290148+Sofia11037@users.noreply.github.com>
2026-01-09 23:28:15 +0100

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -14,22 +14,22 @@ export default function Header() {
           <NavLink to="/">Вакансии</NavLink>
         </div>
         <NavLink to="/" className="logo">
-          <img className="logo-img" src="/image/logo.png" alt="logo" />
+          <img className="logo-img" src="./image/logo.png" alt="logo" />
         </NavLink>
         <div className="nav_right">
           <div className="search">
-            <img src="/icons/search.svg" alt="search" />
+            <img src="./icons/search.svg" alt="search" />
             <span>Поиск</span>
           </div>
           <div className="auth">Вход/Регистрация</div>
           <div className="inter">
-            <img src="/icons/user.svg" alt="user" />
-            <img src="/icons/heart.svg" alt="heart" />
+            <img src="./icons/user.svg" alt="user" />
+            <img src="./icons/heart.svg" alt="heart" />
           </div>
         </div>
         <div className="bar">
           <NavLink to={isFirstClick ? "/select" : "/"} onClick={() => setIsFirstClick(!isFirstClick)}>
-            <img src="/icons/bar.svg" alt="menu" />
+            <img src="./icons/bar.svg" alt="menu" />
           </NavLink>
         </div>
       </div>
